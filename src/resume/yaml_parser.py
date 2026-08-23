c3e5bcd69c3eb8d8436d957eb7ba7a3633b05e65
KedarPanchal <kedar.t.panchal@outlook.com>
2026-08-22 21:10:47 -0500

    Add numerical indexing and ignoring on empty result

--- src/resume/yaml_parser.py
+++ src/resume/yaml_parser.py
@@ -10,43 +10,52 @@ class YamlParser:
         with open(path, 'r') as yaml_file:
             return yaml.safe_load(yaml_file)
     
-    def _load_helper(self, source: dict, key: str, value: dict | None) -> dict:
+    def _load_helper(self, source: dict, key: str, value: dict | None) -> dict | list | str | None:
+        # If empty dict is passed, assume everything should be returned
         if value is None:
-            return source[key] if key in source else {}
+            return source[key] if key in source else None
 
         result = {}
-        if not hasattr(value, "include") or value["include"] == "all":
-                result = source[key] if key in source else {}
+        if "include" not in value or value["include"] == "all":
+                result = source[key] if key in source else None
         elif isinstance(value["include"], list):
             for item in value["include"]:
                 if isinstance(item, str):
                     if item not in source[key]:
                         raise ValueError(f"Item '{item}' not found in source for key '{key}'.")
                     result[item] = source[key][item]
+                elif isinstance(item, int):
+                    target_key = list(source[key].keys())[item]
+                    result[target_key] = source[key][target_key]
                 elif isinstance(item, dict):
                     inner_key = next(iter(item))
                     result[inner_key] = self._load_helper(source[key], inner_key, item[inner_key])
                 else:
-                    raise ValueError(f"Expected a string or dict in 'include' for key '{key}', but got {type(item)}.")
+                    raise ValueError(f"Expected a str, int, or dict in 'include' for key '{key}', but got {type(item)}.")
         else:
             raise ValueError(f"Expected a list or 'all' for 'include' in key '{key}', but got {type(value['include'])}.")
 
-        if hasattr(value, "exclude"):
+        if "exclude" in value:
             if not isinstance(value["exclude"], list) or value["exclude"] == "all":
                 raise ValueError(f"Expected a list for 'exclude' in key '{key}', but got {type(value['exclude'])}.")
             if value["exclude"] == "all":
                 return {}
             exclude_set = set(value["exclude"])
-            result = {k: v for k, v in result.items() if k not in exclude_set}
-        return result
+            if isinstance(result, dict):
+                result = {k: v for k, v in result.items() if k not in exclude_set}
+            elif isinstance(result, list):
+                result = [item for item in result if item not in exclude_set]
+        return result if result else None
 
 
     def load(self, view_path: str):
         view = self._load_dict(view_path)
+        
         result_dict = {}
         for key, value in view.items():
             if not isinstance(value, dict) and value is not None:
                 raise ValueError("Top-level values in the view file must be fields.")
             result = self._load_helper(self._data, key, value)
-            result_dict[key] = result
+            if result is not None:
+                result_dict[key] = result
         return result_dict
