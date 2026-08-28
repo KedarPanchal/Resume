e9b7b136c8ff13be58bca05b92f406034d4e293d
KedarPanchal <kedar.t.panchal@outlook.com>
2026-08-27 23:50:23 -0500

    Update uv project name

--- src/render/__init__.py
+++ src/render/__init__.py
@@ -1,9 +1,15 @@
 from . import yaml_parser
 from . import renderer
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+
     parser = yaml_parser.YamlParser("data.yaml")
     data = parser.load("resume.view.yaml")
-    print(data)
+    logger.info(data)
     latex_renderer = renderer.LatexRenderer("dist/resume.tex")
     latex_renderer.render(data)
